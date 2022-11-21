365168b5954b6c6109a8069c1f98c89cdb629241
Iker Gallastegui <gallastegui.iker@uni.eus>
2022-11-21 11:49:43 +0100

    Adibide ariketa
    1.adibidea bukatuta

---
+++ Adibidea/Adibidea/Controllers/KaixoMunduaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Web;
+
+namespace Adibidea.Controllers
+{
+    public class KaixoMunduaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult OngiEtorri(string izena = "iker", int zenbat = 4)
+        {
+            ViewBag.Mezua = "Hello " + izena;
+            ViewBag.Zenbat = zenbat;
+            return View();
+        }
+    }
+}
