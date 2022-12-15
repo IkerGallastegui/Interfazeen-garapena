268c98710610995a6d2956afb5a46636fcda96dd
Iker Gallastegui <gallastegui.iker@uni.eus>
2022-12-15 10:02:30 +0100

    WineShop

---
+++ WineShop/WineShop/Services/IArdoaService.cs
@@ -0,0 +1,12 @@
+﻿using Wineshop.Models;
+
+namespace Wineshop.Services
+{
+    public interface IArdoaService
+    {
+        Task<IList<ArdoaUpeltegi>> GetMota(int id);
+        Task<List<Ardoa>> GetArdoak();
+        Task<Ardoa> GetArdoa(int id);
+
+    }
+}
---
+++ WineShop/WineShop/Services/ISaskiaService.cs
@@ -0,0 +1,15 @@
+﻿using Wineshop.Models;
+using WineShop.Models;
+
+namespace Wineshop.Services
+{
+    public interface ISaskiaService
+    {
+        Task EskaeraBezeroaGehitu(BezeroaEskaera bezeroaEskaera);
+        Task EskaeraSortu(BezeroaEskaera bezeroaEskaera, string saskiaId);
+        Task SaskiaGehitu(int ardoaId, string saskiaId);
+        Task SaskiaKendu(int id, string saskiaId);
+        Task<List<SaskiaAlea>> SaskiaLortuAleak(string saskiaId);
+
+    }
+}
